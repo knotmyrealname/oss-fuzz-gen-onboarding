9bcbff530e926a71c076b66302371c4ef381c7f0
RamDanny <mrramdhanush362@gmail.com>
2025-11-06 15:21:39 -0500

    Added command parser

--- setup.sh
+++ setup.sh
@@ -4,6 +4,11 @@ mkdir work
 cd work
 git clone https://github.com/knotmyrealname/oss-fuzz-gen.git
 git clone https://github.com/google/oss-fuzz.git
+git clone https://github.com/google/oss-fuzz-gen.git
+cd oss-fuzz-gen
+pip install -r oss-fuzz-gen/requirements.txt
+cd ..
+##TODO SET oss-fuzz-gen/work/oss-fuzz dir to point to the path pwd/oss-fuzz
 
 ## Installing in a virtual environment
 #cd ..
