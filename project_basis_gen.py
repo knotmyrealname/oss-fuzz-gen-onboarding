b8d0411461c891aa820ec778fc079c920c246583
Aiden Lapp <lappaiden@gmail.com>
2025-11-07 01:53:47 -0500

    fixing comment

--- project_basis_gen.py
+++ project_basis_gen.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python3
 # Copyright 2025
-# Licensed under the Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 """
 project_basis_gen.py
 --------------------
-Generates required files for an OSS-Fuzz project  (Dockerfile, project.yaml, build.sh)
-for a given GitHub repository and maintainer email.
+Creates the Dockerfile, project.yaml, and build.sh for a given github repository and maintainer email
 
 Usage:
     python3 project_basis_gen.py <repo_url> <user_email> [work_root]
